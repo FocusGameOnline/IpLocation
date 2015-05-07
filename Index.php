7b7295ee8fcae6d71f6495e066881d8ec26f951c
FocusGameOnline <FocusGameOnline@users.noreply.github.com>
2015-05-07 11:57:25 +0100

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,26 @@
+<?php
+$IPaddr = Dot2LongIP($_GET['IPaddr']);
+
+function Dot2LongIP ($IPaddr)
+{
+    if ($IPaddr == "") {
+        return 0;
+    } else {
+        $ips = split ("\.", "$IPaddr");
+        return ($ips[3] + $ips[2] * 256 + $ips[1] * 256 * 256 + $ips[0] * 256 * 256 * 256);
+    }
+}
+	
+$con=mysqli_connect("LOCALHOST:3306","Admin","Admin157891","ip2location");
+
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$sql = mysqli_query($con,"SELECT country_code, country_name, region_name, city_name FROM ip2location_db3 WHERE $IPaddr BETWEEN IP_FROM AND IP_TO");
+
+while($row = mysqli_fetch_array($sql)) {
+  echo $row['country_code'] . "\n" . $row['country_name'] . "\n" . $row['region_name'] . "\n" . $row['city_name'];
+
+}
+?>
